ac1a1ccf38522d4798104e3d1ea246de5ad89b82
MilicTG <milic.tg@gmail.com>
2021-05-04 19:50:21 +0200

    Initial commit

---
+++ app/src/main/java/dev/milic/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,12 @@
+package dev.milic.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.milic.podplay.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}
