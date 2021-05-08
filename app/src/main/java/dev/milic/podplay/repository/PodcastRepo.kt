93db4f280171c674b53f92d91463b0cb8f609d22
MilicTG <milic.tg@gmail.com>
2021-05-08 13:51:58 +0200

    xml parser

--- app/src/main/java/dev/milic/podplay/repository/PodcastRepo.kt
+++ app/src/main/java/dev/milic/podplay/repository/PodcastRepo.kt
@@ -1,10 +1,18 @@
 package dev.milic.podplay.repository
 
 import dev.milic.podplay.model.Podcast
+import dev.milic.podplay.service.RssFeedService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PodcastRepo {
 
     fun getPodcast(feedUrl: String): Podcast? {
+        val rssFeedService = RssFeedService.instance
+        GlobalScope.launch {
+            rssFeedService.getFeed(feedUrl)
+        }
+
         return Podcast(feedUrl, "No Name", "No description", "No image")
     }
 }
--- app/src/main/java/dev/milic/podplay/util/DateUtils.kt
+++ app/src/main/java/dev/milic/podplay/util/DateUtils.kt
@@ -16,4 +16,10 @@ object DateUtils {
 
         return outputFormat.format(date)
     }
+
+    fun xmlDateToDate(dateString: String?): Date {
+        val date = dateString ?: return Date()
+        val inFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.getDefault())
+        return inFormat.parse(date) ?: Date()
+    }
 }
